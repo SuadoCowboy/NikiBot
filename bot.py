c5f8495bbd347f59ec018745d8052ee850f3bddc
SuadoCowboy <luccacsg@gmail.com>
2025-03-05 23:23:45 -0300

    added nikiscript2 for owners to use

--- bot.py
+++ bot.py
@@ -7,11 +7,14 @@ load_dotenv()
 
 CHANNEL_ID = 1293321381181788261 # allowed channel to use commands
 GUILD_ID = 1293321380863021159
-OWNER_ID = 309806482084462592
 CLIENT_ID = 1346652392350945310
 
+OWNERS_IDS = os.getenv('DISCORD_BOT_OWNERS').split(',')
+for i in range(len(OWNERS_IDS)):
+	OWNERS_IDS[i] = int(OWNERS_IDS[i])
+
 def isOwner(interaction: discord.Interaction):
-	return interaction.user.id == OWNER_ID
+	return interaction.user.id in OWNERS_IDS
 
 nikicmdProcess = subprocess.Popen(
     ["./NikiCMD"],
@@ -56,6 +59,7 @@ def runNikiScript(script: str):
 
 class NikiBot(discord.Client):
 	async def on_ready(self):
+		await tree.sync(guild=discord.Object(GUILD_ID))
 		await tree.sync()
 		print(f'Logged on as {self.user}!')
 
@@ -68,20 +72,30 @@ tree = discord.app_commands.CommandTree(client)
 
 @tree.command(
     name="nikiscript",
-    description="Interprets text as NikiScript"
+    description="Interprets text as NikiScript",
+	guild=discord.Object(GUILD_ID)
 )
-@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 async def script(interaction: discord.Interaction, text: str):
-	if interaction.user.id != OWNER_ID:
-		if interaction.guild_id != GUILD_ID:
-			await interaction.response.send_message("Can't run outside JIKARU lol. Do the L.", ephemeral=True)
-			return
-
-		elif interaction.channel_id != CHANNEL_ID:
-			await interaction.response.send_message(f"Can not run nikiscript here. Try on <#{CHANNEL_ID}> instead.", ephemeral=True)
-			return
+	if not isOwner(interaction) and interaction.channel_id != CHANNEL_ID:
+		await interaction.response.send_message(f"Can not run nikiscript here. Try on <#{CHANNEL_ID}> instead.", ephemeral=True)
+		return
 
 	await interaction.response.defer(ephemeral=interaction.channel_id != CHANNEL_ID, thinking=True)
+
+	embed = discord.Embed(color=discord.Colour(0xffc6ff))
+	embed.add_field(name="In", value=f'```{text}```', inline=False)
+	embed.add_field(name="Out", value=f'```{runNikiScript(text)}```', inline=False)
+
+	await interaction.edit_original_response(embed=embed)
+
+@tree.command(
+	name='nikiscript2',
+	description="Interprets text as NikiScript"
+)
+@discord.app_commands.check(isOwner)
+@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def script(interaction: discord.Interaction, text: str, ephemeral: bool=True):
+	await interaction.response.defer(ephemeral=ephemeral, thinking=True)
 
 	embed = discord.Embed(color=discord.Colour(0xffc6ff))
 	embed.add_field(name="In", value=f'```{text}```', inline=False)
