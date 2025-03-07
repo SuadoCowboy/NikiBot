92a7ab67d9ed99918259122c4f1a373f7afe4bb5
Suado Cowboy <77517394+SuadoCowboy@users.noreply.github.com>
2025-03-07 09:39:42 -0300

    re-arranged order of commands

--- commands.py
+++ commands.py
@@ -1,5 +1,16 @@
 import discord
 
+async def help_command(interaction: discord.Interaction, _command: str, _args: list[str], isOwner: bool):
+	if not interaction.app_permissions.send_messages and not isOwner:
+		return
+
+	out = '```lua\n'
+	for command in commands:
+		out += command+'\n'
+	out += '```'
+
+	await interaction.channel.send(content=out)
+
 async def say_command(interaction: discord.Interaction, command: str, args: list[str], isOwner: bool):
 	if not interaction.app_permissions.send_messages and not isOwner:
 		return
@@ -10,16 +21,4 @@ async def say_command(interaction: discord.Interaction, command: str, args: list
 
 	await interaction.channel.send(content=out.replace('@everyone', 'no u').replace('@here', 'nope'))
 
-async def help_command(interaction: discord.Interaction, _command: str, _args: list[str], isOwner: bool):
-	if not interaction.app_permissions.send_messages and not isOwner:
-		return
-
-	out = '```lua\n'
-	for command in commands:
-		out += command+'\n'
-	out += '```'
-
-	print(out)
-	await interaction.channel.send(content=out)
-
-commands = {'say': say_command, 'help': help_command}
+commands = {'help': help_command, 'say': say_command}
